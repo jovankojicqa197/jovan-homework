bd09adbc4a2de6047c69b952ae6334c6d02bbf2a
jovankojicqa197 <jovankojic7@gmail.com>
2023-12-02 19:26:22 +0100

    1st commit

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+
+  e2e: {
+    specPattern: 'cypress/tests/**/**.{js,jsx,ts,tsx}',
+    supportFile: 'cypress/support/commands.ts',
+    retries: 0,
+    // retries: {runMode: 2, openMode: 0},
+    watchForFileChanges: false,
+    waitForAnimations: true,
+    execTimeout: 60000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    video: false,
+  },
+})
